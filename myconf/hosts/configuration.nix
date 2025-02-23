f45fc20825b6728f3f019851b6b16f8dc5ab97c5
iqbalsonata <iqbalsonata2@gmail.com>
2025-02-23 14:10:21 +0700

    FIXED: update flakes configurations

--- myconf/hosts/configuration.nix
+++ myconf/hosts/configuration.nix
@@ -15,9 +15,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "iqbalsonata"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
+  networking.hostName = "iqbalsonata"; # Define your hostname. networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -150,11 +148,16 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # Enable the Jenkins
+  services.jenkins = {
+    enable = true;
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
